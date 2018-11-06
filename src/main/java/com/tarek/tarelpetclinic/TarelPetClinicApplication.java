fccc7610f98e9f51d3d17a4ca23d284a1b8c27ae
Lamri000 <lamri000@yahoo.fr>
2018-11-06 11:17:24 +0100

    Initial Project Add

---
+++ src/main/java/com/tarek/tarelpetclinic/TarelPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.tarek.tarelpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarelPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( TarelPetClinicApplication.class, args );
+    }
+}

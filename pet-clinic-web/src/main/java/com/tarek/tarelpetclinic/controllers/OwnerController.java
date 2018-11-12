153ff20523c49edd441e5348e9dba0542ff7a465
Lamri000 <lamri000@yahoo.fr>
2018-11-12 08:12:37 +0100

    Create owners index page and controller . closes #13.

---
+++ pet-clinic-web/src/main/java/com/tarek/tarelpetclinic/controllers/OwnerController.java
@@ -0,0 +1,12 @@
+package com.tarek.tarelpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+    @RequestMapping({"/owners"})
+    public String listOwners(){
+        return "owners/index";
+    }
+}
